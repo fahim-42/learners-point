d4b95c627f6624c84d850640614041f19aaa46d0
Fahim Shahriar <https://github.com/fahim-42>
2021-10-04 20:38:53 +0600

    course image added

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Course.css';
 
 const Course = (props) => {
-    const { course, level, price, duration, seat } = props.course;
+    const { image, course, level, price, duration, seat } = props.course;
     return (
         <div className="item border border-2 border-dark rounded rounded-3 m-3 px-3">
-            <h2 className="mt-2 fw-bold fst-italic">{course}</h2>
+            <img style={{height: "200px"}} className="mt-4 img-fluid border rounded-3" src={image} alt="" />
+            <h2 className="mt-3 fw-bold fst-italic">{course}</h2>
             <p className="fs-5 fst-italic">Difficulty: {level}</p>
             <h5 className="mb-3 fst-italic">Price: {price}tk</h5>
             <div className="d-flex">
